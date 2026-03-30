0fe852ddd78901d3990ec35ce41b76669db4bcb2
Pratyay Mustafi <pratyaymustafi@outlook.com>
2026-03-30 19:11:42 +0530

    adding slight changes

--- src/app.d.ts
+++ src/app.d.ts
@@ -6,6 +6,12 @@ declare global {
     interface Locals {}
     interface PageData {}
     interface PageState {}
-    interface Platform {}
+
+    interface Platform {
+      env: Env;
+      ctx: ExecutionContext;
+      caches: CacheStorage;
+      cf?: IncomingRequestCfProperties
+    }
   }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,16 @@
 import { mdsvex } from "mdsvex";
 import { sveltePreprocess } from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    csrf: {
-      trustedOrigins: ["pocketbase.pratyay.qzz.io", "localhost"],
-    },
+    csrf: { trustedOrigins: ["pocketbase.pratyay.qzz.io", "localhost"] }
   },
   preprocess: [mdsvex(), sveltePreprocess()],
   extensions: [".svelte", ".svx"],
-  alias: {
-    $themes: "./src/lib/themes",
-  },
+  alias: { $themes: "./src/lib/themes" }
 };
 
 export default config;
